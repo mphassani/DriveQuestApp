f11dca3cf4aadcec8453a2c1f544099d66bd27e8
Stephanie Munday <laplaces.simulacrum@gmail.com>
2022-02-26 13:16:41 -0800

    updates to intersection page text

--- navigation/screens/IntersectionScreen.js
+++ navigation/screens/IntersectionScreen.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import {
     View,
+    Image,
     Text,
-    ScrollView 
+    ScrollView, 
 } from 'react-native';
 import { Provider as PaperProvider, Button, Appbar, DefaultTheme } from "react-native-paper";
 
@@ -14,7 +15,6 @@ export default function IntersectionScreen({ navigation }) {
 
                 <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', paddingRight: "50%", paddingBottom: "5%", paddingTop: "5%"}}>
                     <Text
-                        //onPress={() => navigation.navigate('Home')}
                         style={{ fontSize: 25, fontWeight: 'bold' }}>Through
                     </Text>
                 </View>
@@ -43,14 +43,66 @@ export default function IntersectionScreen({ navigation }) {
                     </Text>
                 </View>
 
+                
+
+                
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', paddingRight: "60%", paddingBottom: "5%", paddingTop: "5%"}}>
+                    <Text
+                        style={{ fontSize: 25, fontWeight: 'bold' }}>Stop
+                    </Text>
+                </View>
+
+                <View style={{ flex: 1, alignContent: "center", justifyContent: "center", paddingLeft: "20%", paddingBottom: "5%", paddingTop: "5%"}}>
+                    <Text style={{ fontSize: 18}}>
+                        Gap/Limit Line
+                    </Text>
+                </View>
+
+                <View style={{ flex: 1, alignContent: "center", justifyContent: "center", paddingLeft: "20%", paddingBottom: "5%", paddingTop: "5%"}}>
+                    <Text style={{ fontSize: 18}}>
+                        Braking
+                    </Text>
+                </View>
+
+                <View style={{ flex: 1, alignContent: "center", justifyContent: "center", paddingLeft: "20%", paddingBottom: "5%", paddingTop: "5%"}}>
+                    <Text style={{ fontSize: 18}}>
+                        Traffic Check
+                    </Text>
+                </View>
+
+                <View style={{ flex: 1, alignContent: "center", justifyContent: "center", paddingLeft: "20%", paddingBottom: "5%", paddingTop: "5%"}}>
+                    <Text style={{ fontSize: 18}}>
+                        Full Stop
+                    </Text>
+                </View>
+
+
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', paddingRight: "60%", paddingBottom: "5%", paddingTop: "5%"}}>
+                    <Text
+                        style={{ fontSize: 25, fontWeight: 'bold' }}>Start
+                    </Text>
+                </View>
+
+                <View style={{ flex: 1, alignContent: "center", justifyContent: "center", paddingLeft: "20%", paddingBottom: "5%", paddingTop: "5%"}}>
+                    <Text style={{ fontSize: 18}}>
+                        Traffic Check
+                    </Text>
+                </View>
+
+                <View style={{ flex: 1, alignContent: "center", justifyContent: "center", paddingLeft: "20%", paddingBottom: "5%", paddingTop: "5%"}}>
+                    <Text style={{ fontSize: 18}}>
+                        Speed
+                    </Text>
+                </View>
+
+                <View style={{ flex: 1, alignContent: "center", justifyContent: "center", paddingLeft: "20%", paddingBottom: "5%", paddingTop: "5%"}}>
+                    <Text style={{ fontSize: 18}}>
+                        Yield
+                    </Text>
+                </View>
+
 
             </ScrollView>
-            {/* <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text
-                    onPress={() => navigation.navigate('Home')}
-                    style={{ fontSize: 26, fontWeight: 'bold' }}>Intersection Screen
-                </Text>
-            </View> */}
         </PaperProvider>
         
         
