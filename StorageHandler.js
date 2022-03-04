5d97b6193454d813c954f4ddab5d223e6462b0c8
Lucas Torti <47060346+LucTort@users.noreply.github.com>
2022-03-03 17:53:59 -0800

    Update StorageHandler functions, add checkString and checkObject

--- StorageHandler.js
+++ StorageHandler.js
@@ -1,42 +1,68 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const storeStringData = async (part, value, type) => {
-    try {
-      const store = stringify(part + type);
-      await AsyncStorage.setItem("@" + stringify(store), value)
-    } catch (e) {
-      console.log("Exception in DropDown: " + e)
-    }
-    console.log("Finished storing string data as followed: " + part + "/" + value);
-}
-const storeObjectData = async (part, value) => {
-    try {
-      const jsonValue = JSON.stringify(value)
-      await AsyncStorage.setItem("@" + stringify(part), jsonValue)
-    } catch (e) {
-        console.log("Exception in DropDown: " + e)
-    }
-    console.log("Finished storing object data as followed: " + part + "/" + value);
+const storeStringData = async (key, value) => {
+  try {
+    const store = stringify(key);
+    await AsyncStorage.setItem("@" + stringify(store), value);
+  } catch (e) {
+    console.log("Exception in DropDown: " + e);
   }
+  console.log("Finished storing string data as followed: " + key + "/" + value);
+};
+
+const storeObjectData = async (key, value) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem("@" + stringify(key), jsonValue);
+  } catch (e) {
+    console.log("Exception in DropDown: " + e);
+  }
+  console.log("Finished storing object data as followed: " + key + "/" + value);
+};
 
 const getData = async (value) => {
-    try {
-      const value = await AsyncStorage.getItem(value)
-      if(value !== null) {
-        console.log("Attempted to see a value already stored.")
-      }
-    } catch(e) {
-        console.log("Exception in DropDown: " + e)
+  try {
+    const value = await AsyncStorage.getItem(value);
+    if (value !== null) {
+      console.log("Attempted to see a value already stored.");
     }
-    console.log("Finished get string data as followed: " + part + "/" + value);
+  } catch (e) {
+    console.log("Exception in DropDown: " + e);
   }
+  console.log("Finished get string data as followed: " + key + "/" + value);
+};
 
-  const getObjectData = async (value) => {
-    try {
-      const jsonValue = await AsyncStorage.getItem(value)
-      return jsonValue != null ? JSON.parse(jsonValue) : null;
-    } catch(e) {
-        console.log("Exception in DropDown: " + e)
-    }
-    console.log("Finished storing object data as followed: " + part + "/" + value);
+const getObjectData = async (value) => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(value);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    console.log("Exception in DropDown: " + e);
   }
+  console.log("Finished storing object data as followed: " + key + "/" + value);
+};
+
+const checkString = async (key, value) => {
+  try {
+    const store = stringify(key);
+    const keyValue = await AsyncStorage.getItem(store);
+
+    return value == keyValue;
+  } catch (e) {
+    console.log("Exception in checkString: " + e);
+  }
+  console.log("Finished checking data. Returned: " + (value == keyValue));
+};
+
+const checkObject = async (key, value) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    const store = stringify(key);
+    const keyValue = await AsyncStorage.getItem(store);
+
+    return jsonValue == keyValue;
+  } catch (e) {
+    console.log("Exception in checkString: " + e);
+  }
+  console.log("Finished checking data. Returned: " + (jsonValue == keyValue));
+};
