5ccd7f6abc4d111d8b16b5a0f63ad605bc7c5fa3
Stacy Cappelloni <69368458+stacycappelloni@users.noreply.github.com>
2022-03-14 16:58:49 -0700

    Create CheckboxItem.js, remove CheckboxItem from PreDriveScreen.js

--- navigation/screens/PreDriveScreen.js
+++ navigation/screens/PreDriveScreen.js
@@ -4,6 +4,7 @@ import { StyleSheet, SafeAreaView, StatusBar, View, ScrollView, Dimensions, Pres
 import { Provider as PaperProvider, RadioButton, Button, Paragraph, Dialog, Portal, List, Avatar, IconButton, Checkbox, Provider, DefaultTheme} from 'react-native-paper';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import CheckboxItem from "../../components/CheckboxItem";
 
 const { height } = Dimensions.get('window');
 
@@ -253,33 +254,7 @@ export default function PreDriveScreen({ navigation }) {
 
 }
 
-/* Code to create each individual checkbox */
-const CheckboxItem = () => {
 
-    /* Originally, each checkbox is set to notChecked */
-    const [notChecked, setNotChecked] = React.useState(false);
-
-    return (
-        /* Uses checkboxBase style for not checked checkbox, and checkboxChecked for checked checkbox */
-        /* Essentially creates the "box" part of the checkbox */
-        <View style={[styles.checkboxBase, notChecked && styles.checkboxChecked]}>
-            {/* Code that creates the checkbox, counts the number of checkboxes checked to be used in calculating the number of errors */}
-            <Checkbox
-                color="#FFFFFF" status={notChecked ? 'checked' : 'unchecked'}
-                onPress={() => {
-                    setNotChecked(!notChecked);
-                    if (notChecked) {
-                        decrementVal();
-                    }
-                    else {
-                        incrementVal();
-                    }
-                }}
-            />
-        </View>
-
-    );
-};
 
 //counter - counts the number of pre-drive checklist items marked correct for later use in viewing results 
 var counter = 0;
@@ -298,24 +273,6 @@ export function decrementVal() {
 
 //style-sheet 
 const styles = StyleSheet.create({
-    /* Creates outline for the checkbox with no fill; used when item is incorrect */
-    checkboxBase: {
-        width: 60,
-        height: 60,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderWidth: 7,
-        borderRadius: 10,
-        borderColor: '#00677F',
-        backgroundColor: 'transparent',
-        margin: 1.5,
-
-    },
-
-    /* Creates filled in checkbox, used when item is marked correct */
-    checkboxChecked: { //checkbox color 
-        backgroundColor: '#00677F',
-    },
 
     /* creates green circle to use for lefthand icons */
     greenCircle: {
