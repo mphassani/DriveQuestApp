57909b1c17b3f0a0d3ab75213ee739562913838f
Parsa Hassani <78458468+parsa-cu@users.noreply.github.com>
2022-03-14 16:17:58 -0700

    Revert "Looks more like Figma"
    This reverts commit 23e7bc8d996a705c6bad56415caf9836bd886de2.

--- navigation/screens/TestResult.js
+++ navigation/screens/TestResult.js
@@ -1,60 +1,63 @@
 import * as React from 'react';
-import { View, Text,StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 import { List } from 'react-native-paper';
-import { AccordionDisplayer } from './components/AccordionDisplayer';
-import datas from "./DummyData"
 
 
 
-const MyComponent = () => {
-  let sum = 0;
-  let total = 0;
- datas.forEach((data)=>{
-sum = sum + data.completed
-total = total + data.total
-})
-let passed = sum > 70
-  return(
-     <View style={{paddingHorizontal:12}}>
-   {
-     datas.map((data,index)=> {
-      return(
-        <AccordionDisplayer style={{
-          backgroundColor: index % 2 ? '#D6D6D6' : '#F6F6F6'
-        }} title={data.title} completed={data.completed} total={data.total} key={data.title}>  
-          <View>
-          {
-            data.errors.map((error)=>{
-              return(
-                <Text style={{marginVertical:5,marginLeft:5}}>{error.text}</Text>
-              )
-            })
-          }
-          </View>
-        </AccordionDisplayer>
-      )
-     })
-   }
-   <View style={styles.passContainer}>
-   <View style={{flexDirection:'row'}}>
-  <Text>Overall Score: {' '}</Text>
-<Text style={{color:passed ? 'green' :'red'}}>{passed? 'PASS' : 'FAILED'}</Text>
-</View>
-<Text>{sum}/{total}</Text>
-   </View>
- </View>
-  )
-};
 
-const styles = StyleSheet.create({
-  passContainer:{
-    borderRadius:5,
-    backgroundColor:'#cacaca',
-    padding:10,
-flexDirection:'row',
-justifyContent:'space-between',
-marginTop:20
-  }
-})
+const MyComponent = () => (
+  <List.AccordionGroup>
+
+    <List.Accordion title="Pre-Drive Check Results" id="1">
+      <List.Item title="Item 1" />
+    </List.Accordion>
+
+
+    <List.Accordion title="Parking Lot Results" id="2">
+      <List.Item title="Item 2" />
+    </List.Accordion>
+
+
+    <List.Accordion title="Residential Results" id="3">
+      <List.Item title="Item 2" />
+    </List.Accordion>
+
+
+
+    <List.Accordion title="Reversing Results" id="4">
+      <List.Item title="Item 2" />
+    </List.Accordion>
+
+
+
+    <List.Accordion title="Freeway Results" id="5">
+      <List.Item title="Item 2" />
+    </List.Accordion>
+
+
+    <List.Accordion title="Parking Results" id="6">
+      <List.Item title="Item 2" />
+    </List.Accordion>
+
+
+    <List.Accordion title="Turning Results" id="7">
+      <List.Item title="Item 2" />
+    </List.Accordion>
+
+
+    <View>
+     
+      <List.Accordion title="Lane Change Results" id="8">
+        <List.Item title="Item 3" />
+      </List.Accordion>
+       <Text>
+
+
+        Overall Score: PASS/NOT PASS
+      </Text>
+    </View>
+  </List.AccordionGroup>
+
+);
 
 export default MyComponent;
