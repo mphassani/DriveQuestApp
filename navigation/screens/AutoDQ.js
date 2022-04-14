1b0e6621f4e52803c37e60b6f208b4a10e48f497
Mark Reiland <80435916+MarkReiland@users.noreply.github.com>
2022-04-13 17:41:22 -0700

    Added new Auto DQ page

--- navigation/screens/AutoDQ.js
+++ navigation/screens/AutoDQ.js
@@ -1,95 +1,99 @@
 import * as React from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
-import { Provider as PaperProvider, RadioButton, Button, Paragraph, Dialog, Portal, Provider, Divider, Checkbox, Title } from 'react-native-paper';
+import { StyleSheet, SafeAreaView, StatusBar, View, ScrollView, Dimensions, Pressable, Text, Image, TextInput } from 'react-native';
+import { Provider as PaperProvider, RadioButton, Button, Paragraph, Dialog, Portal, List, Avatar, IconButton, Checkbox, Provider, DefaultTheme} from 'react-native-paper';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import * as StorageHandler from "../../StorageHandler";
 import HomeScreen from './HomeScreen';
 import MainHome from '../../AllScreen';
-import { ScrollView } from 'react-native-gesture-handler';
+import CheckboxItem from "../../components/CheckboxItem";
 
 export default function AutoDQ({ navigation }) {
 
     const navigator = useNavigation();
-    const [visible, setVisible] = React.useState(true);
-    const hideDialog = () => setVisible(false);
-
-    const [checked, setChecked] = React.useState();
-
-    function checkTheBox(dq) {
-        StorageHandler.storeStringData("AUTO_DQ", dq);
-    }
 
     return (
         <PaperProvider>
-            <ScrollView>
-                <View style={{ justifyContent: 'center', alignContent: 'center', flexDirection: 'row' }}>
-                    <Title style={{ paddingTop: '5%' }}>Automatic Disqualifaction</Title>
-                </View>
-                <Checkbox.Item
-                    label="Examiner Intervention"
-                    value="first"
-                    color="#90C96A"
-                    status={checked === 'first' ? 'checked' : 'unchecked'}
-                    onPress={() => { setChecked('first'); checkTheBox("Examiner Intervention"); }}
-                />
-                <Checkbox.Item
-                    label="Dangerous Maneuver"
-                    value="second"
-                    color="#90C96A"
-                    status={checked === 'second' ? 'checked' : 'unchecked'}
-                    onPress={() => { setChecked('second'); checkTheBox("Dangerous Maneuver"); }}
-                />
-                <Checkbox.Item
-                    label="Strikes Object"
-                    value="third"
-                    color="#90C96A"
-                    status={checked === 'third' ? 'checked' : 'unchecked'}
-                    onPress={() => { setChecked('third'); checkTheBox("Strikes Object"); }}
-                />
-                <Checkbox.Item
-                    label="Driving Speed"
-                    value="fourth"
-                    color="#90C96A"
-                    status={checked === 'fourth' ? 'checked' : 'unchecked'}
-                    onPress={() => { setChecked('fourth'); checkTheBox("Driving Speed"); }}
-                />
-                <Checkbox.Item
-                    label="Disobeys Traffic Signage"
-                    value="fifth"
-                    color="#90C96A"
-                    status={checked === 'fifth' ? 'checked' : 'unchecked'}
-                    onPress={() => { setChecked('fifth'); checkTheBox("Disobeys Traffic Signage"); }}
-                />
-                <Checkbox.Item
-                    label="Aux Equipment Use"
-                    value="sixth"
-                    color="#90C96A"
-                    status={checked === 'sixth' ? 'checked' : 'unchecked'}
-                    onPress={() => { setChecked('sixth'); checkTheBox("Aux Equipment Use"); }}
-                />
-                <Checkbox.Item
-                    label="Disobeys Examiner"
-                    value="seventh"
-                    color="#90C96A"
-                    status={checked === 'seventh' ? 'checked' : 'unchecked'}
-                    onPress={() => { setChecked('seventh'); checkTheBox("Disobeys Examiner"); }}
-                />
-                <Checkbox.Item
-                    label="Lane Violation"
-                    value="eigth"
-                    color="#90C96A"
-                    status={checked === 'eigth' ? 'checked' : 'unchecked'}
-                    onPress={() => { setChecked('eigth'); checkTheBox("Lane Violation"); }}
-                />
-                {/* <Dialog.Actions>
-                    <Button color="#12414F" onPress={() => { StorageHandler.storeStringData("AUTO_DQ", dq); }}>Submit</Button>
-                    <Button color="#12414F" onPress={() => { hideDialog; navigator.goBack() }}>Cancel</Button>
-                </Dialog.Actions> */}
-            </ScrollView>
+            <SafeAreaView>
+                {/* Enables the scroll bar feature for the page */}
+                <ScrollView>
+                    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', paddingRight: "10%", paddingTop: "5%" }}>
+                        {/* Sets properties for the title text */}
+                        <Text
+                            style={{ fontSize: 25, fontWeight: 'bold' }}>Automatic Disqualification
+                        </Text>
+                    </View>
+                    <List.Section>
 
+                        <List.Item
+                            title=" Examiner Intervention"
+                            left={(props) => <Image style={styles.grayCircle} source={require('../../assets/driverSideWindow.png')} />}
+                            right={() => <CheckboxItem storageKey="AUTODQ_EXAMINER_INTERVENTION" />}
+                        />
 
+                        <List.Item
+                            title=" Dangerous Maneuver"
+                            left={(props) => <Image style={styles.grayCircle} source={require('../../assets/driverSideWindow.png')} />}
+                            right={() => <CheckboxItem storageKey="AUTODQ_DANGEROUS_MANEUVER" />}
+                        />
+
+                        <List.Item
+                            title=" Strikes Object"
+                            left={(props) => <Image style={styles.grayCircle} source={require('../../assets/driverSideWindow.png')} />}
+                            right={() => <CheckboxItem storageKey="AUTODQ_STRIKES_OBJECT" />}
+                        />
+
+                        <List.Item
+                            title=" Driving Speed"
+                            left={(props) => <Image style={styles.grayCircle} source={require('../../assets/driverSideWindow.png')} />}
+                            right={() => <CheckboxItem storageKey="AUTODQ_DRIVING_SPEED" />}
+                        />
+
+                        <List.Item
+                            title=" Disobeys Traffic Signage"
+                            left={(props) => <Image style={styles.grayCircle} source={require('../../assets/driverSideWindow.png')} />}
+                            right={() => <CheckboxItem storageKey="AUTODQ_DISOBEYS_TRAFFIC_SIGNAGE" />}
+                        />
+
+                        <List.Item
+                            title=" Aux Equipment Use"
+                            left={(props) => <Image style={styles.grayCircle} source={require('../../assets/driverSideWindow.png')} />}
+                            right={() => <CheckboxItem storageKey="AUTODQ_AUX_EQUIPMENT_USE" />}
+                        />
+
+                        <List.Item
+                            title=" Disobeys Examiner"
+                            left={(props) => <Image style={styles.grayCircle} source={require('../../assets/driverSideWindow.png')} />}
+                            right={() => <CheckboxItem storageKey="AUTODQ_DISOBEYS_EXAMINER" />}
+                        />
+
+                        <List.Item
+                            title=" Lane Violation"
+                            left={(props) => <Image style={styles.grayCircle} source={require('../../assets/driverSideWindow.png')} />}
+                            right={() => <CheckboxItem storageKey="AUTODQ_LANE_VIOLATION" />}
+                        />
+
+                    </List.Section>
+                </ScrollView>
+            </SafeAreaView>
         </PaperProvider>
+
     );
 
+
+
+
 }
+
+
+//style-sheet 
+const styles = StyleSheet.create({
+
+    /* creates gray circle to use for lefthand icons */
+    grayCircle: {
+        height: 64,
+        width: 64, 
+        borderRadius: 32, 
+        backgroundColor: "#707070",
+    },
+});
